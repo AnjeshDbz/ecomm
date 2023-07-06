61a3b054af561f42eb5a42e65b66a31f429721ea
Anjesh Kushwaha <anjesh51227@gmail.com>
2023-07-06 13:42:14 +0530

    auth guard

---
+++ src/app/seller-auth/seller-auth.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from "@angular/core";
+import { SellerService } from "../services/seller.service";
+import { Router } from "@angular/router";
+import { SignUp } from "../data-type";
+@Component({
+  selector: "app-seller-auth",
+  templateUrl: "./seller-auth.component.html",
+  styleUrls: ["./seller-auth.component.css"],
+})
+export class SellerAuthComponent implements OnInit {
+  constructor(private seller: SellerService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.seller.reloadSeller();
+  }
+
+  signUp(data: SignUp): void {
+    // console.log(data);
+    // this.seller.userSignUp(data).subscribe((result) => {
+    //   if (result) {
+    //     this.router.navigate(["seller-home"]);
+    //   }
+    // });
+
+    this.seller.userSignUp(data)
+  }
+}
