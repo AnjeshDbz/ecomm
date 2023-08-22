d0bfcdb41c9c67741e7f587a81016723cee003c0
Anjesh Kushwaha <anjesh51227@gmail.com>
2023-08-22 12:03:46 +0530

    add product

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,6 +7,7 @@ import { Router } from "@angular/router";
 })
 export class HeaderComponent {
   menuType: string = "default";
+  sellerName: string = "";
   constructor(private route: Router) {}
   ngOnInit(): void {
     this.route.events.subscribe((val: any) => {
@@ -14,11 +15,20 @@ export class HeaderComponent {
         if (localStorage.getItem("seller") && val.url.includes("seller")) {
           console.log("In Seller area");
           this.menuType = "seller";
+          if (localStorage.getItem("seller")) {
+            let sellerStore = localStorage.getItem("seller");
+            let sellerData = sellerStore && JSON.parse(sellerStore)[0];
+            this.sellerName = sellerData.name;
+          }
         } else {
           console.log("outside seller");
           this.menuType = "default";
         }
       }
     });
+  }
+  logout() {
+    localStorage.removeItem("seller");
+    this.route.navigate(["/"]);
   }
 }
