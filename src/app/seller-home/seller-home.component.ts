6260028f919a6bfbaec484e4fe701af0e323eb62
Anjesh Kushwaha <anjesh51227@gmail.com>
2023-08-22 15:52:13 +0530

    Display Product List with API

--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ProductService } from "../services/product.service";
+import { product } from "../data-type";
 
 @Component({
-  selector: 'app-seller-home',
-  templateUrl: './seller-home.component.html',
-  styleUrls: ['./seller-home.component.css']
+  selector: "app-seller-home",
+  templateUrl: "./seller-home.component.html",
+  styleUrls: ["./seller-home.component.css"],
 })
 export class SellerHomeComponent {
-
+  productList: undefined | product[];
+  constructor(private product: ProductService) {}
+  ngOnInit(): void {
+    this.product.productList().subscribe((result) => {
+      console.log(result);
+      this.productList = result;
+    });
+  }
 }
