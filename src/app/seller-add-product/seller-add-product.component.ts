73c18a718426a0acf971a228ad9d523b693f1bf5
Anjesh Kushwaha <anjesh51227@gmail.com>
2023-08-22 14:13:42 +0530

    Add Product API Integration

--- src/app/seller-add-product/seller-add-product.component.ts
+++ src/app/seller-add-product/seller-add-product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { ProductService } from "../services/product.service";
+import { product } from "../data-type";
 
 @Component({
   selector: "app-seller-add-product",
@@ -6,9 +8,17 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./seller-add-product.component.css"],
 })
 export class SellerAddProductComponent implements OnInit {
-  constructor() {}
+  addProductMessage: string | undefined;
+  constructor(private product: ProductService) {}
   ngOnInit(): void {}
-  submit(data: object) {
-    console.log(data);
+  submit(data: product) {
+    // console.log(data);
+    this.product.addProduct(data).subscribe((result) => {
+      console.log(result);
+      if (result) {
+        this.addProductMessage = "Product is successfully added";
+      }
+      setTimeout(() => (this.addProductMessage = undefined), 3000);
+    });
   }
 }
